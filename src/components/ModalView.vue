5276c45e1dbdbb4a75b6fe321e2334f181a5fecc
wilkersoh <plgu@hotmail.com>
2021-04-25 14:38:09 +0800

    update: keyboard hook

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -8,9 +8,18 @@
 </template>
 
 <script>
+import useKeydown from '@/composable/useKeydown';
+
   export default {
     setup(props, {emit}) {
-      //
+      useKeydown([
+        /* 需要 cb， 因為 是過後 才call的，
+          如果沒有 cb, 根本 就是 不對的 javascript
+          fn: console.log('dsds');
+        */
+        {key: 'Escape', fn: () => emit('closeModal')},
+        {key: 'Enter', fn: () => console.log("tryrys")}
+      ]);
     }
   }
 </script>
