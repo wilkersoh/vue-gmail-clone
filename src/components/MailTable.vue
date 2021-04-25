7a45310a7dd77517e4b02adb82a810157edf892b
wilkersoh <plgu@hotmail.com>
2021-04-25 14:00:34 +0800

    update: show click email msg

--- src/components/MailTable.vue
+++ src/components/MailTable.vue
@@ -6,7 +6,7 @@
       <tr v-for="email in unarchivedEmails"
         :key="email.id"
         :class="['clickable', email.read ? 'read' : '']"
-        @click="email.read = true"
+        @click="openEmail(email)"
       >
         <td>
           <input type="checkbox">
@@ -16,27 +16,34 @@
           <p><strong>{{ email.subject }}</strong>- {{ email.body }}</p>
         </td>
         <td class="date">{{ format(new Date(email.sentAt), 'MMM do yyyy') }}</td>
-        <td><button @click="email.archived = true">Archive</button></td>
+        <td><button @click="archiveEmail(email)">Archive</button></td>
       </tr>
     </tbody>
   </table>
-  </div>
+  <MailView v-if="openedEmail" :email="openedEmail" />
+
+</div>
 </template>
 
 <script>
   import {computed, reactive, toRefs} from 'vue';
   import {format} from 'date-fns';
   import axios from "axios";
+  import MailView from "@/components/MailView.vue"
+
+  const Endpoint = "http://localhost:3000/emails";
 
   export default {
+    components: {MailView},
     async setup() {
       const state = reactive({
         emails: [],
+        openedEmail: null,
         error: "",
       });
       try {
         await new Promise(res => setTimeout(res, 2000))
-        const {data} = await axios("http://localhost:3000/emails");
+        const {data} = await axios(Endpoint);
         state.emails = data;
       } catch (error) {
         state.error = error.message;
@@ -50,9 +57,25 @@
         return filtered;
       })
 
+      const updateEmail = (email) =>{
+        axios.put(`${Endpoint}/${email.id}`, email)
+      }
+
+      const openEmail = (email) => {
+        email.read = true;
+        updateEmail(email);
+        state.openedEmail = email
+      }
+      const archiveEmail = (email) => {
+        email.archived = true;
+        updateEmail(email);
+      }
+
       return {
         format,
         unarchivedEmails,
+        archiveEmail,
+        openEmail,
         ...toRefs(state),
       }
     },
---
+++ src/components/MailView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="email-display">
+    <h2 class="mb-0">Subject: <strong>{{ email.subject }}</strong></h2>
+    <div><em>{{ email.from }} on {{ format(new Date(email.sentAt), 'MMM do yyy') }}</em></div>
+    <div v-html="marked(email.body)" />
+  </div>
+</template>
+
+<script>
+  import { format } from 'date-fns';
+  import marked from 'marked'
+
+  export default {
+    props: {
+      email: {
+        type: Object,
+        required: true
+      }
+    },
+    setup(props) {
+      return {
+        format,
+        marked
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
